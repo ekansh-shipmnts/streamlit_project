63c046c13f3f546c8f2cf4ef45f77a71a8679205
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2023-12-02 10:57:19 +0530

    ...

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
