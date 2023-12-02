9bb9ea532f3e516adef6ab82ae35ce681cc48c3f
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2023-12-02 10:55:21 +0530

    ...

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-from streamlit_webrtc import webrtc_streamer
-webrtc_streamer(key="sample")
+import cv2
+import streamlit as st
+
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
