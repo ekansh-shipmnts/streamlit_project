e80b05bce74d3a24bd3645a1917f3eddaacf86b4
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2023-12-02 11:27:14 +0530

    ...

--- test.py
+++ test.py
@@ -1,5 +1,82 @@
-from streamlit_webrtc import webrtc_streamer,WebRtcMode, RTCConfiguration
-RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-webrtc_streamer(key="sample",rtc_configuration=RTC_CONFIGURATION,mode=WebRtcMode.SENDRECV,media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,)
+import cv2
+import mediapipe as mp
+import numpy as np
+import streamlit as st
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+@st.cache(allow_output_mutation=True)
+def init_pose_model():
+    return mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    if angle > 180.0:
+        angle = 360 - angle
+    return angle
+
+def main():
+    st.title("Pose Detection App")
+    
+    pose_model = init_pose_model()
+    cap = cv2.VideoCapture(0)
+
+    counter = 0
+    stage = None
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.warning("Unable to capture video stream.")
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = pose_model.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        try:
+            landmarks = results.pose_landmarks.landmark
+            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                        landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            shoulder1 = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow1 = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                   landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+
+            angle = calculate_angle(shoulder, elbow, wrist)
+            angle1 = calculate_angle(elbow1, shoulder1, hip)
+
+            cv2.putText(image, f"Angle 1: {angle}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
+                        cv2.LINE_AA)
+            cv2.putText(image, f"Angle 2: {angle1}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
+                        cv2.LINE_AA)
+
+            if angle > 100 and angle1 < 50:
+                stage = "down"
+            if angle < 30 and angle1 < 50 and stage == 'down':
+                stage = "up"
+                counter += 1
+                st.success(f"Rep Count: {counter}")
+
+        except:
+            pass
+
+        st.image(image, channels="BGR", use_column_width=True)
+
+if _name_ == "_main_":
+    main()
