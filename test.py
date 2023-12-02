17d376a4d7ff1bab427905f1fae6371ae51d4be4
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2023-12-02 11:14:05 +0530

    ...

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import cv2
-import streamlit as st
-
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
-
-while run:
-    _, frame = camera.read()
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+from streamlit_webrtc import webrtc_streamer,WebRtcMode, RTCConfiguration
+RTC_CONFIGURATION = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+webrtc_streamer(key="sample",rtc_configuration=RTC_CONFIGURATION,mode=WebRtcMode.SENDRECV,media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,)
