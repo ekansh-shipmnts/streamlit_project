986e0a1789cad4f5cb048af00cf0356033a3b66f
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2023-12-02 11:41:56 +0530

    ...

--- test.py
+++ test.py
@@ -2,43 +2,22 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-@st.cache(allow_output_mutation=True)
-def init_pose_model():
-    return mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+class PoseVideoTransformer(VideoTransformerBase):
+    def _init_(self):
+        self.counter = 0
+        self.stage = None
+        self.pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-def calculate_angle(a, b, c):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
-    if angle > 180.0:
-        angle = 360 - angle
-    return angle
-
-def main():
-    st.title("Pose Detection App")
-    
-    pose_model = init_pose_model()
-    cap = cv2.VideoCapture(0)
-
-    counter = 0
-    stage = None
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            st.warning("Unable to capture video stream.")
-            break
-
+    def transform(self, frame):
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
 
-        results = pose_model.process(image)
+        results = self.pose.process(image)
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -61,22 +40,42 @@ def main():
             angle = calculate_angle(shoulder, elbow, wrist)
             angle1 = calculate_angle(elbow1, shoulder1, hip)
 
-            cv2.putText(image, f"Angle 1: {angle}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
-                        cv2.LINE_AA)
-            cv2.putText(image, f"Angle 2: {angle1}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2,
-                        cv2.LINE_AA)
+            cv2.putText(image, str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, str(angle1),
+                        tuple(np.multiply(shoulder1, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
             if angle > 100 and angle1 < 50:
-                stage = "down"
-            if angle < 30 and angle1 < 50 and stage == 'down':
-                stage = "up"
-                counter += 1
-                st.success(f"Rep Count: {counter}")
+                self.stage = "down"
+            if angle < 30 and angle1 < 50 and self.stage == 'down':
+                self.stage = "up"
+                self.counter += 1
+                print(self.counter)
 
         except:
             pass
 
-        st.image(image, channels="BGR", use_column_width=True)
+        cv2.rectangle(image, (0, 0), (250, 80), (245, 117, 16), -1)
 
-if __name__ == '__main__':
-    main()
+        cv2.putText(image, 'REPS', (15, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(self.counter),
+                    (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'STAGE', (85, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, self.stage,
+                    (75, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+        return image
+
+
+webrtc_streamer(key="example", video_transformer_factory=PoseVideoTransformer)
